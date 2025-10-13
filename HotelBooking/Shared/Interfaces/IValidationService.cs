cb7ebfbefc7556493289f5b72912d8d99d01c358
Kristian Urup <kristian@eliteit.dk>
2025-10-13 09:59:02 +0200

    Adding tests for Validation of User

---
+++ HotelBooking/Shared/Interfaces/IValidationService.cs
@@ -0,0 +1,14 @@
+﻿using Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Interfaces
+{
+    public interface IValidationService
+    {
+        ValidationResponse ValidateNoNullOrEmpty<T>(T model, List<string> propNames);
+    }
+}
